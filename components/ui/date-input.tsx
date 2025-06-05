32984edad04d43341b73d4d8567a18a6e79c4ceb
mharry1997 <107109724+mharry1997@users.noreply.github.com>
2025-06-05 23:58:39 +0100

    Pruning

--- components/ui/date-input.tsx
+++ components/ui/date-input.tsx
@@ -1,29 +1,30 @@
-import React from "react";
-import { FormControl, FormLabel } from "@chakra-ui/form-control";
-import { Input } from "@chakra-ui/react"
+import { Field, Input } from "@chakra-ui/react"
+import React from "react"
 
 interface DateInputProps {
-  label: string;
-  value: string;
-  onChange: (val: string) => void;
-  isRequired?: boolean;
+  label: string
+  value: string
+  onChange: (val: string) => void
+  isRequired?: boolean
 }
 
 export const DateInput: React.FC<DateInputProps> = ({
-  label,
-  value,
-  onChange,
-  isRequired,
-}) => {
+                                                      label,
+                                                      value,
+                                                      onChange,
+                                                      isRequired,
+                                                    }) => {
   return (
-    <FormControl mb={4} isRequired={isRequired}>
-      <FormLabel color="gray.300" mb={5}>{label}</FormLabel>
+    <Field.Root required={isRequired}>
+      <Field.Label color="gray.300" mb={1}>
+        {label}
+      </Field.Label>
       <Input
         type="date"
-        color="gray.400"
         value={value}
         onChange={(e) => onChange(e.target.value)}
+        color="gray.400"
       />
-    </FormControl>
-  );
-};
+    </Field.Root>
+  )
+}
